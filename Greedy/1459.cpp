9dca4e66415e8959f3d8d656dacb6ffb8ad362d0
aqswa <69039161+aqswa@users.noreply.github.com>
2023-02-22 05:36:09 +0900

    boj 1459

---
+++ Greedy/1459.cpp
@@ -0,0 +1,29 @@
+//
+// Created by LG on 2023-02-22.
+//
+#include <iostream>
+
+using namespace  std;
+
+int main(){
+    long long x, y, w, s;
+
+    cin >> x >> y >> w >> s;
+
+    if(x < y){
+        swap(x, y);
+    }
+
+    long long ans = 0;
+    if(s < w*2)
+        ans += s*y;
+    else
+        ans += 2*w*y;
+
+    if(s < w)
+        ans += 2*s*((x-y)/2) + w*((x-y)%2);
+    else
+        ans += w*(x-y);
+
+    cout << ans;
+}
