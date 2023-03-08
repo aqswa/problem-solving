e10b86b8cbc37568b0dca1dfdf1f96ea08c87eac
aqswa <joayoung3@gmail.com>
2023-03-08 15:10:19 +0900

    [Greedy] 회문

--- Greedy/17609.cpp
+++ Greedy/17609.cpp
@@ -3,55 +3,42 @@
 //
 #include <iostream>
 
-using namespace  std;
+using namespace std;
 
 int solve(string str){
     int flag = 0;
     int s = 0, e = str.length()-1;
 
-    while(true) {
-        if(s >= e){
-            return flag;
-        }
-
-        if (str[s] == str[e]) {
-            s++;
-            e--;
-            continue;
-        }
-
-        if (str[s + 1] == str[e] && flag == 0) {
-            if(s+1 == e){
-                flag = 1;
-                return flag;
-            }
-            else if (str[s + 2] == str[e - 1]) {
-                flag = 1;
-                s += 3;
-                e -= 2;
-                continue;
-            }
-        }
-
-        if (str[s] == str[e - 1] && flag == 0) {
-            if(s == e-1){
-                flag = 1;
-                return flag;
-            }
-            else if (str[s + 1] == str[e - 2]) {
-                flag = 1;
-                s += 2;
-                e -= 3;
-            } else {
-                flag = 2;
-                return flag;
-            }
-        }
-        else{
-            flag = 2;
-            return flag;
+    for(int i=0; i<=e/2; i++){
+        if(str[i] != str[e-i]){
+            s = i;
+            flag = 1;
+            break;
         }
     }
+
+    if(flag){
+        e -= s;
+        int mid = (e-s)/2;
+        for(int i=0; i<mid; i++){
+            if(str[s+i+1] != str[e-i]){
+                flag = 2;
+                break;
+            }
+        }
+
+        if(flag == 2){
+            for(int i=0; i<mid; i++){
+                if(str[s+i] != str[e-i-1]){
+                    flag = 2;
+                    break;
+                }
+                flag = 1;
+            }
+        }
+    }
+
+    return flag;
 }
 
 int main(){
